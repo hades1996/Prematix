9f3def55295a621bd62d3cec96d7202937630aa2
hades1996 <48794592+hades1996@users.noreply.github.com>
2021-03-22 16:48:32 -0500

    modificacion.pediatra

--- prematix.Web/Data/Entities/Pediatra.cs
+++ prematix.Web/Data/Entities/Pediatra.cs
@@ -10,15 +10,22 @@
     {
 		public int Id { get; set; }
 
+		[MaxLength(50, ErrorMessage ="este campo no debe tener mas de 50 caracteres")]
+		[Required(ErrorMessage ="El Campo {0} es obligatorio")]
+		[Display(Name = "Nombre y Apellido")]
 		public string Name { get; set; }
 
+		[Required(ErrorMessage = "El Campo {0} es obligatorio")]
 		public string Cedula { get; set; }
 
+		[Required(ErrorMessage = "El Campo {0} es obligatorio")]
 		public string Entidad { get; set; }
 
+		[Required(ErrorMessage = "El Campo {0} es obligatorio")]
 		public string Telefono { get; set; }
 
 		[Display(Name = "Image")]
+		[Required(ErrorMessage = "El Campo {0} es obligatorio")]
 		public string ImageUrl { get; set; }
 
 	}
---
+++ prematix.Web/Migrations/20210322212150_ModifyProducts.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace prematix.Web.Migrations
+{
+    public partial class ModifyProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Pediatras",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pediatras",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Pediatras",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Entidad",
+                table: "Pediatras",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cedula",
+                table: "Pediatras",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Pediatras",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pediatras",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Pediatras",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Entidad",
+                table: "Pediatras",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cedula",
+                table: "Pediatras",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
