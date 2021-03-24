a9d6df1e1421628acee4527ca5ee7b7949277cfb
hades1996 <48794592+hades1996@users.noreply.github.com>
2021-03-24 16:00:00 -0500

    API

---
+++ prematix.Web/Controllers/API/PediatrasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prematix.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prematix.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+
+    public class PediatrasController : Controller
+    {
+        private readonly IPediatraRepository pediatraRepository;
+
+        public PediatrasController(IPediatraRepository pediatraRepository)
+        {
+            this.pediatraRepository = pediatraRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetPediatras()
+        {
+
+            return Ok(this.pediatraRepository.GetAll());
+        }
+    }
+}
